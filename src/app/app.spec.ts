2fa3bb26723fa01d8be6f59f10853d55bad17a6f
Natasha Arez <natasha.arez@fleetcomplete.com>
2025-10-29 04:16:10 -0400

    update

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, order-management-suite');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Purchase Order Management Suite');
   });
 });
